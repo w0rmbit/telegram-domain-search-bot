0eaf9decd0270a20990e27b58e16b940d0f36446
w0rmbit <123961968+w0rmbit@users.noreply.github.com>
2025-09-09 19:00:38 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-CHANNEL_ID = os.getenv("CHANNEL_ID")  # Optional: restrict uploads
+CHANNEL_ID = os.getenv("CHANNEL_ID")
